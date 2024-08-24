79c3f3a9da3fdf9bf4eeb5ea9478c8eaba56bba9
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-08-24 23:45:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-constructo
+
+class Symbol {
+    constructor(){
+
+    }
+    draw(){
+
+    }
+}
