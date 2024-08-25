bbf1d2c01dc923480c077ef01d022696b8af3cb9
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-08-25 22:47:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 class Symbol {
-    constructor(){
-
+    constructor(x, y, fontSize,canvasHeight){
+        this.characters = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        this.x = x;
+        this.y = y;
+        this.fontSize = fontSize;
+        this.text = '';
+        this.canvasHeight;
     }
     draw(){
 
@@ -17,7 +22,7 @@ class Effect {
         this.canvasWidth = canvasWidth;
         this.canvasHeight = canvasHeight
         this.fontSize =25;
-        this.colums = this.columnWidth/this.fontSize;
+        this.columns = this.columnWidth/this.fontSize;
         this.symbols = [];
     }
     #initialize(){
