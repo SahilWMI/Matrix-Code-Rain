ade4ef7b0ca8786d5436b869c4e6342a6413f68c
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-08-26 23:08:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let gradient =ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+gradient.addColorStop(0, 'red');
+gradient.addColorStop(0.2, 'yellow');
+gradient.addColorStop(0.4, 'green');
+gradient.addColorStop(0.6, 'cyan');
+gradient.addColorStop(0.8, 'blue');
+gradient.addColorStop(1, 'magenta');
+
 class Symbol {
     constructor(x, y, fontSize, canvasHeight){
         this.characters = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -39,18 +47,18 @@ class Effect {
         }
     }
     resize(width, height){
-        this.canvasWidth = canvasWidth;
-        this.canvasHeight = canvasHeight;
+        this.canvasWidth = width;
+        this.canvasHeight = height;
         this.fontSize = 25;
         this.columns = this.canvasWidth / this.fontSize;
         this.symbols = [];
-        this.#initialize
+        this.#initialize();
     }
 }
 
 const effect = new Effect(canvas.width, canvas.height);
 let lastTime = 0;
-const fps = 30;
+const fps = 60;
 const nextFrame = 1000/fps;
 let timer = 0;
 
@@ -61,13 +69,14 @@ function animate(timeStamp){
         ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
         ctx.textAlign = 'center';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = '#0aff0a';
+        ctx.fillStyle = gradient; //'#0aff0a';
         ctx.font = effect.fontSize + 'px monospace';
         effect.symbols.forEach(symbol => symbol.draw(ctx));
         timer = 0;
     } else {
         timer += deltaTime;
     }
+
     requestAnimationFrame(animate);
 }
 
